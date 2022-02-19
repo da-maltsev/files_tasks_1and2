912db92370ad5b210597a3b1083f19e8f4bff837
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2022-02-19 11:39:14 +0500

    task_1

---
+++ task_1_2.py
@@ -0,0 +1,14 @@
+import os
+def nice_cook_book(recipies):
+    with open(recipies, encoding='utf-8') as file:
+        cook_book = {}
+        for txt in file.read().split('\n\n'):
+            name, _, *args = txt.split('\n')
+            tmp = []
+            for arg in args:
+                ingredient_name, quantity, measure = map(lambda x: int(x) if x.isdigit() else x, arg.split(' | '))
+                tmp.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
+            cook_book[name] = tmp
+        return print(cook_book)
+
+nice_cook_book('recipies.txt')
