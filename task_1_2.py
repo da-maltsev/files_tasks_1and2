924694a78a54694735af27d5763990937c1ad6d8
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2022-02-19 11:53:10 +0500

    taks_2

--- task_1_2.py
+++ task_1_2.py
@@ -1,4 +1,3 @@
-import os
 def nice_cook_book(recipies):
     with open(recipies, encoding='utf-8') as file:
         cook_book = {}
@@ -9,6 +8,34 @@ def nice_cook_book(recipies):
                 ingredient_name, quantity, measure = map(lambda x: int(x) if x.isdigit() else x, arg.split(' | '))
                 tmp.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
             cook_book[name] = tmp
-        return print(cook_book)
+        return (cook_book)
+
 
 nice_cook_book('recipies.txt')
+
+
+def get_shop_list_by_dishes(dishes, persons):
+    menu = nice_cook_book('recipies.txt')
+    print('Наше меню выглядит вот так:')
+    print(menu)
+    print()
+    shopping_list = {}
+    try:
+        for dish in dishes:
+            for item in (menu[dish]):
+                items_list = dict([(item['ingredient_name'],\
+                                    {'measure': item['measure'], 'quantity': int(item['quantity'])*persons})])
+                if shopping_list.get(item['ingredient_name']):
+                    extra_item = (int(shopping_list[item['ingredient_name']]['quantity']) +
+                                  int(items_list[item['ingredient_name']]['quantity']))
+                    shopping_list[item['ingredient_name']]['quantity'] = extra_item
+
+                else:
+                    shopping_list.update(items_list)
+        print(f"Для приготовления блюд на {persons} человек  нам необходимо купить:")
+        print(shopping_list)
+    except KeyError:
+        print("Вы ошиблись в названии блюда, проверьте ввод")
+
+
+get_shop_list_by_dishes(['Омлет', 'Омлет', 'Фахитос'], 1)
